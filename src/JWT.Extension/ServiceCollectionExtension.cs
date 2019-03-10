df1db549eab94df66e3ba9c2756983e91ac5699c
Owen <315872858@qq.com>
2019-03-10 11:03:56 +0800

    dev

---
+++ src/JWT.Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JWT.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddJWT(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+    }
+}
